a6640eb25ee163506248bc6b2f652a92bf2f3533
Sasha Skryd <sashaskryd@gmail.com>
2021-10-10 18:55:17 +0300

    add pages

---
+++ client/src/pages/AuthPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const AuthPage = () => {
+  return (
+    <div className="row">
+      <div className="col s6 offset-s3">
+        <h1>Shorten the link</h1>
+      </div>
+    </div>
+  );
+};
